89eb65d70cf14455c02985a65110eff9926add35
Vinay-Kashyap8326 <vinay2911kashyap@gmail.com>
2026-06-05 14:55:48 +0530

    Add Indian Currancy

--- Currency_Converter.py
+++ Currency_Converter.py
@@ -1,18 +1,19 @@
 def language(items) :
   for i in range(len(items)): # all elements of list print karne ke liye wo bhi serial NO. ke sath
     print(i+1,"-",items[i])
-lan_List = ["US Dollar (USD)","Euro (EUR)","Japanese Yen (JPY)","British Pound (GBP)","Chinese Yuan (CNY)","Swiss Franc (CHF)","Australian Dollar (AUD)","Canadian Dollar (CAD)","Hong Kong Dollar (HKD)","Singapore Dollar (SGD)"]
+lan_List = ["US Dollar (USD)","Euro (EUR)","Japanese Yen (JPY)","British Pound (GBP)","Chinese Yuan (CNY)","Swiss Franc (CHF)","Australian Dollar (AUD)","Canadian Dollar (CAD)","Hong Kong Dollar (HKD)","Singapore Dollar (SGD)","Indian Rupee (INR)"]
 price_list = { # isse me rates ko easily access kar sakta hu
-  "US Dollar (USD)" : [1,0.86,159.70,0.74,6.77,0.79,1.40,1.38,7.84,1.28],
-  "Euro (EUR)" : [1.16,1,185.45,0.86,7.85,0.91,1.62,1.60,9.10,1.48],
-  "Japanese Yen (JPY)" : [0.0063,0.0054,1,0.0047,0.0424,0.0049,0.0088,0.0087,0.0491,0.0080],
-  "British Pound (GBP)" : [1.3423,1.1555,214.38,1,9.0824,1.0574,1.8799,1.8584,10.5194,1.7175],
-  "Chinese Yuan (CNY)" : [0.1478,0.1273,23.60,0.1101,1,0.1164,0.2070,0.2046,1.1582,0.1891],
-  "Swiss Franc (CHF)" : [1.2695,1.0929,202.73,0.9456,8.5901,1,1.7785,1.7577,9.9491,1.6247],
-  "Australian Dollar (AUD)" : [0.7141,0.6147,114.03,0.5319,4.8315,0.5623,1,0.9883,5.5942,0.9135],
-  "Canadian Dollar (CAD)" : [0.7222,0.6219,115.34,0.5381,4.8879,0.5689,1.0118,1,5.6599,0.9242],
-  "Hong Kong Dollar (HKD)" : [0.1276,0.1098,20.38,0.0951,0.8634,0.1005,0.1787,0.1767,1,0.1630],
-  "Singapore Dollar (SGD)" : [0.7814,0.6729,124.79,0.5822,5.2871,0.6155,1.0946,1.0818,6.1235,1]
+  "US Dollar (USD)" : [1,0.86,159.70,0.74,6.77,0.79,1.40,1.38,7.84,1.28,95.05],
+  "Euro (EUR)" : [1.16,1,185.45,0.86,7.85,0.91,1.62,1.60,9.10,1.48,110.59],
+  "Japanese Yen (JPY)" : [0.0063,0.0054,1,0.0047,0.0424,0.0049,0.0088,0.0087,0.0491,0.0080,0.59],
+  "British Pound (GBP)" : [1.3423,1.1555,214.38,1,9.0824,1.0574,1.8799,1.8584,10.5194,1.7175,127.92],
+  "Chinese Yuan (CNY)" : [0.1478,0.1273,23.60,0.1101,1,0.1164,0.2070,0.2046,1.1582,0.1891,14.03],
+  "Swiss Franc (CHF)" : [1.2695,1.0929,202.73,0.9456,8.5901,1,1.7785,1.7577,9.9491,1.6247,120.62],
+  "Australian Dollar (AUD)" : [0.7141,0.6147,114.03,0.5319,4.8315,0.5623,1,0.9883,5.5942,0.9135,67.82],
+  "Canadian Dollar (CAD)" : [0.7222,0.6219,115.34,0.5381,4.8879,0.5689,1.0118,1,5.6599,0.9242,68.44],
+  "Hong Kong Dollar (HKD)" : [0.1276,0.1098,20.38,0.0951,0.8634,0.1005,0.1787,0.1767,1,0.1630,12.13],
+  "Singapore Dollar (SGD)" : [0.7814,0.6729,124.79,0.5822,5.2871,0.6155,1.0946,1.0818,6.1235,1,74.02],
+  "Indian Rupee (INR)" : [0.011,0.0090,1.68,0.0078,0.071,0.0083,0.015,0.015,0.082,0.014,1]
 }
 iftrue=True
 while iftrue: # agar user correction karna chahta hai to wo easily kar sakta hai
@@ -44,6 +45,7 @@ while iftrue: # agar user correction karna chahta hai to wo easily kar sakta hai
       amount=float(input("\nEnter Amount: ")) # input float bhi ho sakta hai isley float me type cast kar diya
       current_price = price_list[option1][input2-1] # ye rate ko access kar raha hai jisme user ko convert karna hai
       print("\n1",option1,"=>",current_price,option2) # yaha rate show kar diya hai 
+      print("\nYour Result---")
       print(f"\n{amount:.2f} {option1} => {(amount*current_price):.2f} {option2}\n") # yaha fstring ka use kiya hai jisse float value 2 decimals tak hi print kare or result bhi 2 decimals tak hi diya
       iftrue= False # outer loop ko rokne ke liye
       break # ye hone ke baad inner loop close
