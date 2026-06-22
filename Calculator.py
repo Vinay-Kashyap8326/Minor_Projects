52c70f330a3d54786c612e1fde44f4c18647ce3f
Vinay-Kashyap8326 <vinay2911kashyap@gmail.com>
2026-06-22 15:05:15 +0530

    Start Project

--- Calculator.py
+++ Calculator.py
@@ -1,1 +1,12 @@
-print("Hello")
+print("--- Calculator ---\n")
+print("What You Want To Do ?\n")
+Operetors = ["Addition", "Subtraction", "Division", "Multiplication"]
+i = 0
+while(i < len(Operetors)) :
+  print(i+1,"- ",Operetors[i])
+  i+=1
+Operator_Input = int(input("\nEnter Option: "))
+if((Operator_Input >= 0) and (Operator_Input <= len(Operetors))) :
+  print("Done")
+else :
+  print("Invalid Option")
