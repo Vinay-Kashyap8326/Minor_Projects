80626ff58f0d5761e321371197646ad21b669f89
Vinay-Kashyap8326 <vinay2911kashyap@gmail.com>
2026-06-24 16:45:18 +0530

    In Proccess

--- Calculator.py
+++ Calculator.py
@@ -7,6 +7,29 @@ while(i < len(Operetors)) :
   i+=1
 Operator_Input = int(input("\nEnter Option: "))
 if((Operator_Input >= 0) and (Operator_Input <= len(Operetors))) :
-  pass
+  match Operator_Input: 
+    case 1: 
+      n1 = int(input("\nEnter 1st Number: "))
+      n2 = int(input("\nEnter 2nd Number: "))
+      print("\nAnswer is :",n1,"+",n2,"=",n1+n2)
+    case 2: 
+      n1 = int(input("\nEnter 1st Number: "))
+      n2 = int(input("\nEnter 2nd Number: "))
+      print("\nAnswer is :",n1,"-",n2,"=",n1-n2)
+    case 3: 
+      n1 = int(input("\nEnter 1st Number: "))
+      n2 = int(input("\nEnter 2nd Number: "))
+      if(n2!=0):
+        print("\nAnswer is :",n1,"÷",n2,"=",n1/n2)
+      else : 
+        print("\nCannot Divided By Zero")
+    case 4: 
+      n1 = int(input("\nEnter 1st Number: "))
+      n2 = int(input("\nEnter 2nd Number: "))
+      print("\nAnswer is :",n1,"×",n2,"=",n1*n2)
+    case _:
+      print("Invalid Option")
+  
 else :
   print("Invalid Option")
+  
