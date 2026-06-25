eecb2a928e65e2cfa8de772ffe9286c95bae2861
Vinay-Kashyap8326 <vinay2911kashyap@gmail.com>
2026-06-25 09:23:43 +0530

    Complete Calculator Project

--- Calculator.py
+++ Calculator.py
@@ -5,31 +5,29 @@ i = 0
 while(i < len(Operetors)) :
   print(i+1,"- ",Operetors[i])
   i+=1
-Operator_Input = int(input("\nEnter Option: "))
-if((Operator_Input >= 0) and (Operator_Input <= len(Operetors))) :
-  match Operator_Input: 
-    case 1: 
+try:
+  Operator_Input = int(input("\nEnter Option: "))
+  if((Operator_Input > 0) and (Operator_Input <= len(Operetors))) :
+    if(Operator_Input==1):
       n1 = int(input("\nEnter 1st Number: "))
       n2 = int(input("\nEnter 2nd Number: "))
-      print("\nAnswer is :",n1,"+",n2,"=",n1+n2)
-    case 2: 
+      print(f"\nAnswer is : {n1} + {n2} = {n1+n2}")
+    elif(Operator_Input==2):
       n1 = int(input("\nEnter 1st Number: "))
       n2 = int(input("\nEnter 2nd Number: "))
-      print("\nAnswer is :",n1,"-",n2,"=",n1-n2)
-    case 3: 
+      print(f"\nAnswer is : {n1} - {n2} = {n1-n2}")
+    elif(Operator_Input==3):
+      try:
+        n1 = int(input("\nEnter 1st Number: "))
+        n2 = int(input("\nEnter 2nd Number: "))
+        print(f"\nAnswer is : {n1} ÷ {n2} = {n1/n2}")
+      except ZeroDivisionError: 
+        print("\nERROR: Cannot Divided By Zero")
+    elif(Operator_Input==4):
       n1 = int(input("\nEnter 1st Number: "))
       n2 = int(input("\nEnter 2nd Number: "))
-      if(n2!=0):
-        print("\nAnswer is :",n1,"÷",n2,"=",n1/n2)
-      else : 
-        print("\nCannot Divided By Zero")
-    case 4: 
-      n1 = int(input("\nEnter 1st Number: "))
-      n2 = int(input("\nEnter 2nd Number: "))
-      print("\nAnswer is :",n1,"×",n2,"=",n1*n2)
-    case _:
-      print("Invalid Option")
-  
-else :
-  print("Invalid Option")
-  
+      print(f"\nAnswer is : {n1} × {n2} = {n1*n2}")
+  else :
+    print("\nInvalid Option")
+except ValueError:
+  print("\nInvalid Character")
