cd71cc17aaf7dc5ddbd96716e2c03cd71daee6aa
Vinay-Kashyap8326 <vinay2911kashyap@gmail.com>
2026-06-27 16:28:29 +0530

    Optimize Calculator Project

--- Calculator.py
+++ Calculator.py
@@ -1,33 +1,33 @@
 print(f"{'--- Calculator ---\n':^53}")
 print("What You Want To Do ?\n")
 Operetors = ["Addition", "Subtraction", "Division", "Multiplication"]
-i = 0
-while(i < len(Operetors)) :
-  print(i + 1,"- ",Operetors[i])
-  i += 1
-try:
-  Operator_Input = int(input("\nEnter Option: "))
-  if((Operator_Input > 0) and (Operator_Input <= len(Operetors))) :
-    if(Operator_Input == 1):
+iftrue = True
+while(iftrue) :
+  i = 0
+  while(i < len(Operetors)) :
+    print(i + 1,"- ",Operetors[i])
+    i += 1
+  try:# Again waka banao and comments ache se likho
+    Operator_Input = int(input("\nEnter Option: "))
+    if((Operator_Input > 0) and (Operator_Input <= len(Operetors))) :
       n1 = int(input("\nEnter 1st Number: "))
       n2 = int(input("\nEnter 2nd Number: "))
-      print(f"\nAnswer is : {n1} + {n2} = {n1+n2}")
-    elif(Operator_Input == 2):
-      n1 = int(input("\nEnter 1st Number: "))
-      n2 = int(input("\nEnter 2nd Number: "))
-      print(f"\nAnswer is : {n1} - {n2} = {n1-n2}")
-    elif(Operator_Input == 3):
-      try:
-        n1 = int(input("\nEnter 1st Number: "))
-        n2 = int(input("\nEnter 2nd Number: "))
-        print(f"\nAnswer is : {n1} ÷ {n2} = {n1/n2}")
-      except ZeroDivisionError: 
-        print("\nERROR: Cannot Divided By Zero")
-    elif(Operator_Input == 4):
-      n1 = int(input("\nEnter 1st Number: "))
-      n2 = int(input("\nEnter 2nd Number: "))
-      print(f"\nAnswer is : {n1} × {n2} = {n1*n2}")
-  else :
-    print("\nInvalid Option")
-except ValueError:
-  print("\nInvalid Character")
+      if(Operator_Input == 1):
+        print(f"\nAnswer is : {n1} + {n2} = {n1+n2}")
+        iftrue = False
+      elif(Operator_Input == 2):
+        print(f"\nAnswer is : {n1} - {n2} = {n1-n2}")
+        iftrue = False
+      elif(Operator_Input == 3):
+        try:
+          print(f"\nAnswer is : {n1} ÷ {n2} = {n1/n2}")
+          iftrue = False
+        except ZeroDivisionError: 
+          print("\nERROR: Cannot Divided By Zero\n")
+      elif(Operator_Input == 4):
+        print(f"\nAnswer is : {n1} × {n2} = {n1*n2}")
+        iftrue = False
+    else :
+      print("\nInvalid Option\n")
+  except ValueError:
+    print("\nInvalid Character\n")
