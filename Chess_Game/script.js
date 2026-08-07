1291c89b5d69d35b3e9f772c0cc994bffa96f7d7
Vinay-Kashyap8326 <vinay2911kashyap@gmail.com>
2026-08-07 19:23:32 +0530

    Creating chess Game

---
+++ Chess_Game/script.js
@@ -0,0 +1,8 @@
+let whitePieces = document.querySelectorAll(".whitePiecesBox .whitePiece");
+let blackPieces = document.querySelectorAll(".blackPiecesBox .blackPiece");
+let clicked = true;
+whitePieces.forEach((whitePiece) => {
+    whitePieces.addEventListener("click",() => {
+        console.log("hello");
+    });
+});
