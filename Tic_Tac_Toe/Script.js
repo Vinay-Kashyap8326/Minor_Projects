ad6507ee6691454505a653dbdbd8f4090d0b62a1
Vinay-Kashyap8326 <vinay2911kashyap@gmail.com>
2026-08-09 20:16:07 +0530

    Add win lose logic and remove repeatendense

--- Tic_Tac_Toe/Script.js
+++ Tic_Tac_Toe/Script.js
@@ -1,19 +1,31 @@
 let buttons = document.querySelectorAll(".button");
+let msg = document.querySelector(".status p");
 let turn = true; //turn flipping.
-const winConditons = [[0,3,6],[1,4,7],[2,5,8],[0,1,2],[3,4,5],[6,7,8],[0,4,8],[3,4,6]];
-let winChecker = (index) => {
-
+const winConditons = [[0, 3, 6], [1, 4, 7], [2, 5, 8], [0, 1, 2], [3, 4, 5], [6, 7, 8], [0, 4, 8], [2, 4, 6]];
+let winChecker = () => {
+  for (let i of winConditons) {
+    let pos1 = buttons[i[0]].innerText;
+    let pos2 = buttons[i[1]].innerText;
+    let pos3 = buttons[i[2]].innerText;
+    if (pos1 != "" && pos2 != "" && pos3 != "") {
+      if (pos1 === pos2 && pos2 === pos3) {
+        msg.innerText = `Winner Is ${pos1}`;
+      }
+    }
+  }
 }
 buttons.forEach((buttons) => {
-    buttons.addEventListener("click",() => {
-        if(turn){
-            buttons.innerText = "X";
-            buttons.style.color = "rgb(114, 113, 113)";
-            turn = false; //Now player 2's turn
-        } else {
-            buttons.innerText = "O";
-            buttons.style.color = "rgba(204, 204, 204)";
-            turn = true;//Now player 1's turn
-        };
-    });
+  buttons.addEventListener("click", () => {
+    if (turn) {
+      buttons.innerText = "X";
+      buttons.style.color = "rgb(114, 113, 113)";
+      turn = false; //Now player O turn
+    } else {
+      buttons.innerText = "O";
+      buttons.style.color = "rgba(204, 204, 204)";
+      turn = true; //Now player X turn
+    };
+    buttons.disabled = true;
+    winChecker();
+  });
 });
